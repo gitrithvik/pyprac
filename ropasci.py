5540a162b6d058064970d4d6ea284f4724151c10
gitrithvik <gitrithvik@gmail.com>
2017-09-25 20:24:55 +0530

    Added Ro Pa Sci countdown of 3 seconds

--- ropasci.py
+++ ropasci.py
@@ -1,5 +1,5 @@
-#! usr/bin/python
-import random, sys
+#! /usr/bin/python
+import random, sys ,time
 """Implement simple rock paper scissors"""
 
 options = ["Rock","Paper","Scissors"]
@@ -66,12 +66,22 @@ while rounds % 2 ==0:
 	print "Please enter an odd number of rounds"
 	rounds = int(get_rounds())
 total = 0
+
+"""Start the game"""
+
 while rounds > 0:
 	
 		print "Please enter your choice of:"
 		for i in options:
 			print i
 		choice = raw_input("Please type in the choice correctly: ")
+		print "Let's Go! "
+		time.sleep(1)
+		print "Rock"
+		time.sleep(1)
+		print "Paper"
+		time.sleep(1)
+		print "Scissors"
 		if choice == "Rock" or choice[0] == "R" or choice[0] == "r":
 			total=playRock(total)
 			print total
@@ -82,6 +92,9 @@ while rounds > 0:
 			total=playScissors(total)
 			print total
 		rounds-=1
+
+"""Result"""
+
 if total > 0:
 	print "You won!!"
 elif total < 0:
